a1205dc969f3bd1e126cb6b575c0f75eb53a5345
oculis <oculis0925@yonsei.ac.kr>
2023-10-24 18:05:28 +0900

    20231024_180500

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
+const _$ = document.createElement.bind(document);
 
 const list = $('#nx_doctor .cht_list');
-const addUserChat = () => {
+const addUserChat = inner => {
+    const chat = _$('li');
+    chat.classList.add('cht_user');
+    const txt = _$('div');
+    txt.classList.add('cht_txt');
+    txt.innerHTML = inner;
+
+}
+
+const addDocChat = inner => {
 
 }
 
